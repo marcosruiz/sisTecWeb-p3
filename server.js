e90d27c160d06a5558a137617b5e0dded46d43ff
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2016-08-13 21:16:35 +0200

    Tutorial 2 hecho
    ahora se pueden subir imagenes

--- server.js
+++ server.js
@@ -3,21 +3,10 @@ var url = require("url");
 
 function start(route, handle){
 	function onRequest(request, response){
-		var postData = "";
+
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-
-		request.setEncoding("utf8");
-
-		request.addListener("data", function(postDataChunk){
-		  // called when a new chunk of data was received
-		  postData = postData + postDataChunk;
-		});
-
-		request.addListener("end", function(){
-		  // called when all chunks of data have been received
-			route(handle, pathname, response, postData);
-		});
+		route(handle, pathname, response, request);
 
 	}
 	http.createServer(onRequest).listen(8888);
